39a4a16956cf55f7b0875d912034ca861273a535
David21092 <bischof.david.db@gmail.com>
2023-06-28 15:14:20 +0200

    About us added

---
+++ src/Pages/Aboutus.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../css/Aboutus.css'
+
+export const Aboutus = () => {
+  return (
+    <div id="about-us">
+      <br />
+      <h2>Willkommen bei Natural Aquatics!</h2>
+      <p>
+        <br />
+        Natural Aquatics ist ein Unternehmen, das sich auf den Verkauf von Naturwasser spezialisiert hat. Wir glauben an die Schönheit und Reinheit der Natur und möchten Ihnen dabei helfen, die erfrischendsten und gesündesten Wasserverbindungen der Welt zu entdecken.
+        <br />
+        <br />
+        Unser Unternehmen wurde mit dem Ziel gegründet, Menschen Zugang zu hochwertigem Naturwasser zu verschaffen. Wir sind stolz darauf, Ihnen eine breite Auswahl an natürlichen Wasserprodukten anzubieten, die aus unberührten Quellen und Gewässern stammen. Jedes Produkt in unserem Sortiment wird sorgfältig ausgewählt, um Ihnen ein außergewöhnliches Geschmackserlebnis und eine gesunde Hydration zu bieten.
+        <br />
+        <br />
+        Bei Natural Aquatics legen wir großen Wert auf Nachhaltigkeit und Umweltschutz. Wir arbeiten eng mit lokalen Gemeinschaften und Organisationen zusammen, um sicherzustellen, dass unsere Wasserressourcen geschützt und nachhaltig genutzt werden. Wir setzen uns für den Erhalt der natürlichen Lebensräume ein, aus denen unser Wasser stammt, und unterstützen Projekte zur Wiederherstellung und Aufrechterhaltung der ökologischen Vielfalt.
+        <br />
+        <br />
+        Diese Website wurde von David Bischof im Rahmen eines Schulprojekts im Fach Prog1 erstellt. David ist ein engagierter Schüler mit einer Leidenschaft für Natur und Umwelt. Er hat sich zum Ziel gesetzt, das Bewusstsein für die Bedeutung von sauberem und gesundem Wasser zu fördern. Durch dieses Projekt möchte er Natural Aquatics dabei unterstützen, seine Botschaft zu verbreiten und Menschen für die Schönheit und den Wert der Naturwasserressourcen zu sensibilisieren.
+        <br />
+        <br />
+        Vielen Dank, dass Sie Natural Aquatics besuchen. Wir laden Sie ein, unsere Produkte zu erkunden und ein erfrischendes Stück Natur in Ihr Leben zu bringen. Für Fragen und Anregungen stehen wir Ihnen jederzeit zur Verfügung.
+        <br />
+        <br />
+        Ihr Natural Aquatics Team
+      </p>
+    </div>
+  )
+}
+
+export default Aboutus
--- src/Pages/WrapperPage.jsx
+++ src/Pages/WrapperPage.jsx
@@ -1,9 +1,10 @@
 import Home from './Home.jsx'
 import Beforefter from './Beforeafter.jsx'
 import Imagegallery from './Imagegallery.jsx'
+import Aboutus from './Aboutus.jsx'
+import Contact from './Contact'
 // import Help from '../components/FAQ';
 // import About from "./About"
-import Contact from './Contact'
 import { Route, Routes } from 'react-router-dom'
 import React from 'react'
 import Navbar from '../components/Navbar.jsx'
@@ -16,7 +17,7 @@ export default function WrapperPage () {
                 <Route path='/plans' element={<Navbar />} />
                 <Route path='/gallery' element={<Imagegallery />} />
                 <Route path='/beforeAfter' element={<Beforefter />} />
-                <Route path='/about' element={<Navbar />} />
+                <Route path='/about' element={<Aboutus />} />
                 <Route path='/faq' element={<Navbar />} />
                 <Route path='/contact' element={<Contact />} />
             </Routes>
